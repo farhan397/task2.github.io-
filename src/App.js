c18cc2f313dc423dbb302945a3adb9433bec5537
farhan397 <123460462+farhan397@users.noreply.github.com>
2023-03-20 17:57:42 +0500

    comit122

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import React,{useState,useEffect} from 'react'
-
-import Imgcard from './Imgcard/Imgcard';
-import Datafilter from './DataFilter/Datafilter';
+import Foam1 from './component/Foam1';
 
 
 function App() {
-  
-
-
- 
-  
   return (
     <div className="App">
-      <div>
-        <Datafilter/>
-      </div>
-      <div>
-        <Imgcard/>
-      </div>
+      <Foam1/>
     </div>
   );
 }
