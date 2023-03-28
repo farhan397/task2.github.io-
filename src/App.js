92ace8731db13e9f4eca776703049f2523dd107a
farhan397 <123460462+farhan397@users.noreply.github.com>
2023-03-28 15:33:38 +0500

    comit8

--- src/App.js
+++ src/App.js
@@ -1,14 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
-import Foam1 from './component/Foam1';
+import React, { useState } from 'react';
+import { Container } from 'react-bootstrap';
+import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import Home from './components/MovieList/MovieList';
+import './App.css'
 
 
-function App() {
-  return (
-    <div className="App">
-      <Foam1/>
-    </div>
-  );
+
+
+
+
+const App = () => {
+
+    const [searchValue, setSearchValue] = useState('')
+    const [name, setName] = useState('')
+
+
+
+
+    const handeSubmit = () => {
+        if (name !== '') {
+            setSearchValue(name)
+            setName('')
+        }
+        else {
+
+            alert('Enter Movie Name')
+        }
+    }
+
+    const handel = (e) => {
+        let value = e.target.value
+        setName(value)
+    }
+
+    const handelClear = () => {
+        setSearchValue('')
+        setSearchValue(name)
+
+    }
+
+    return (
+        <>
+
+            <Navbar bg="light" expand="lg">
+                <Container fluid>
+                    <div className='maindiv mt-1   p-2 pb-4 '>
+                        <Navbar.Brand className='text-white  headfont mt-3'>Search Movies</Navbar.Brand>
+                        <div className='fielddiv'> <Form.Control className='headersfild Headerborders '
+                            onChange={handel}
+                            name='name'
+                            value={name} />
+                            <button onClick={handeSubmit} className='mt-3 bg-primary Headerborders  headerButton'>Search</button>
+                        </div>
+                    </div>
+                </Container></Navbar>
+
+            <Home searchValue={searchValue} handelClear={handelClear} />
+
+
+        </>
+    )
 }
 
-export default App;
+export default App
+
+
+
+
